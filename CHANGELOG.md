786e7bd904bc86b7ee785c4bb2ce47a0179f0d5b
Frank Hoppe <hoppe.berlin@googlemail.com>
2020-09-28 13:29:44 +0200

    * Funktion pagePicker auskommentiert in Klasse Vereinsregister

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # DSB-Vereinsregister Changelog
 
+## Version 1.0.2 (2020-09-28)
+
+* Funktion pagePicker auskommentiert in Klasse Vereinsregister
+
 ## Version 1.0.1 (2019-06-27)
 
 * Fix: composer.json falsches Bundle verlinkt
--- src/Classes/Vereinsregister.php
+++ src/Classes/Vereinsregister.php
@@ -61,10 +61,10 @@ class Vereinsregister
 	 * @param \DataContainer
 	 * @return string
 	 */
-	public static function pagePicker(DataContainer $dc)
-	{
-		return ' <a href="contao/page.php?do=' . Input::get('do') . '&amp;table=' . $dc->table . '&amp;field=' . $dc->field . '&amp;value=' . str_replace(array('{{link_url::', '}}'), '', $dc->value) . '" title="' . specialchars($GLOBALS['TL_LANG']['MSC']['pagepicker']) . '" onclick="Backend.getScrollOffset();Backend.openModalSelector({\'width\':765,\'title\':\'' . specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['MOD']['page'][0])) . '\',\'url\':this.href,\'id\':\'' . $dc->field . '\',\'tag\':\'ctrl_'. $dc->field . ((Input::get('act') == 'editAll') ? '_' . $dc->id : '') . '\',\'self\':this});return false">' . Image::getHtml('pickpage.gif', $GLOBALS['TL_LANG']['MSC']['pagepicker'], 'style="vertical-align:top;cursor:pointer"') . '</a>';
-	}
+	//public static function pagePicker(DataContainer $dc)
+	//{
+	//	return ' <a href="contao/page.php?do=' . Input::get('do') . '&amp;table=' . $dc->table . '&amp;field=' . $dc->field . '&amp;value=' . str_replace(array('{{link_url::', '}}'), '', $dc->value) . '" title="' . specialchars($GLOBALS['TL_LANG']['MSC']['pagepicker']) . '" onclick="Backend.getScrollOffset();Backend.openModalSelector({\'width\':765,\'title\':\'' . specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['MOD']['page'][0])) . '\',\'url\':this.href,\'id\':\'' . $dc->field . '\',\'tag\':\'ctrl_'. $dc->field . ((Input::get('act') == 'editAll') ? '_' . $dc->id : '') . '\',\'self\':this});return false">' . Image::getHtml('pickpage.gif', $GLOBALS['TL_LANG']['MSC']['pagepicker'], 'style="vertical-align:top;cursor:pointer"') . '</a>';
+	//}
 
 
 }
